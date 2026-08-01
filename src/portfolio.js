38e690733c6491128432f23e8eb2bcd08cf0a71b
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2026-08-02 01:18:47 +0700

    Refactor portfolio.js to improve code readability and efficiency

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,8 +23,7 @@ const greeting = {
   nickname: "tuf-tuf / leguna",
   subTitle:
     "Developer that passionate at game dev and mobile app dev. Love Building AI Bot, but hate Data Cleaning.",
-  resumeLink:
-    "https://drive.google.com/file/d/1ilwOgmy7_1Cub0qOBgfI6k-NHC9A3XeI/view?usp=sharing",
+  resumeLink: "https://drive.google.com/drive/u/0/my-drive",
   portfolio_repository: "https://github.com/Leguna/leguna.github.io/",
 };
 
